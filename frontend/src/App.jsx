b7840264a348cf1de0f7c2ec54f331cf442ddbf6
3idhun <midhunmohanm2002@gmail.com>
2025-06-04 16:41:21 +0530

    new

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,23 @@
-
-import Navbar from './assets/components/Navbar';
-import Homepage from './pages/Homepage';
+import { Route, Routes } from "react-router";
+import Navbar from "./components/Navbar";
+import Homepage from "./pages/Homepage";
+import Moviepage from "./pages/Moviepage";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
 
 const App = () => {
   return (
     <div>
       <Navbar />
-      <Homepage />
-    </div>
-  )
-}
 
-export default App
+      <Routes>
+        <Route path={"/"} element={<Homepage />} />
+        <Route path={"/movie/:id"} element={<Moviepage />} />
+        <Route path={"/signin"} element={<SignIn />} />
+        <Route path={"/signup"} element={<SignUp />} />
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
